1307797715220abb9cc6fe69d0c9a12ed3a61a27
Francisco-Robles <franciscojrobles26@gmail.com>
2022-08-16 20:53:01 -0300

    eureka

--- ms-bike/src/main/java/com/tutorial/msbike/MsBikeApplication.java
+++ ms-bike/src/main/java/com/tutorial/msbike/MsBikeApplication.java
@@ -2,8 +2,10 @@ package com.tutorial.msbike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsBikeApplication {
 
 	public static void main(String[] args) {
